1167db6e38b6e36d9b0dcd36db17e9492961451c
MacDuPain <godel.Sacha@gmail.com>
2024-04-29 17:25:18 +0200

    ajout des pages potins, des pages users et ajout de style

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @first_name = "Jean-Mich"
+    @gossips = Gossip.all
+    @users = User.includes(:gossips)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-
   get 'contact', to: 'contact#contact'
   get 'team', to: 'team#team'
   get 'welcome/:first_name', to: 'welcome#index', as: 'welcome'
+  get 'gossip/:id', to: 'gossip#show', as: 'gossip'
+  get 'user/:id', to: 'user#show', as: 'user'
+
+  resources :gossips, only: [:index, :show]
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
